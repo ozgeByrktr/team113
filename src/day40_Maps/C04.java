b91874a33978b04fc625fd76ab7c9ccc08355df7
Ozge Bayraktar <byrktr.oske@gmail.com>
2023-04-02 14:27:37 +0300

    02/04/2023

---
+++ src/day40_Maps/C04.java
@@ -0,0 +1,22 @@
+package day40_Maps;
+
+import java.util.Map;
+
+public class C04 {
+    public static void main(String[] args) {
+        // verilen okul map'inde
+        // ogrencilerin siniflarini bir artirin
+        // eger 12.sinif varsa, sinif bilgisi olarak mezun yazin
+        // eger update varsa, key ve value birlikte dusunulmelidir
+        // ayni key ile yeni value'yu map'e ekledigimizde
+        // key unique olacagi icin eski value'yu update edip yeni value degerini kaydeder
+
+        Map<Integer,String> okulMap = MapMethodDepo.ornekMapOlustur();
+        System.out.println(okulMap);
+        okulMap = MapMethodDepo.yilSonuSiniflariDegistir(okulMap);
+        System.out.println(okulMap);
+        // denemek icin bir kere daha calistiralim
+        okulMap = MapMethodDepo.yilSonuSiniflariDegistir(okulMap);
+        System.out.println(okulMap);
+    }
+}
