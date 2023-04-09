afccc72ef71e96c505c28374f0540471fd401a76
Ozge Bayraktar <byrktr.oske@gmail.com>
2023-04-09 14:24:30 +0300

    09/04/2023Java The End

--- src/day43_HashSet/C02_TekrarEdenElemanlar.java
+++ src/day43_HashSet/C02_TekrarEdenElemanlar.java
@@ -1,5 +1,8 @@
 package day43_HashSet;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class C02_TekrarEdenElemanlar {
     public static void main(String[] args) {
            /*
@@ -9,5 +12,12 @@ public class C02_TekrarEdenElemanlar {
         int [] arr={1,2,1,3,4,5,1,2,6,2,3,4,5,1,3,2,6,5,7}
          */
         int [] arr={1,2,1,3,4,5,1,2,6,2,3,4,5,1,3,2,6,5,7};
+
+        Set<Integer>liste=new HashSet<>();
+        for (Integer each:arr
+             ) {
+            liste.add(each);
+        }
+        System.out.println(liste);
     }
 }
